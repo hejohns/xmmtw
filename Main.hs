b37f2df50cd4341add0003bf58ba3f4ca60d51e5
Johnson He <hejohns@umich.edu>
2021-03-06 08:58:09 +0000

    lmao gtk is hilarious
    also i finally understand overloaded labels.
    I guess you can use the same method to simulate OO function overloading
    (I assume the gi had to write an instance for each gobject, which
     is why it's autogenerated)

--- Main.hs
+++ Main.hs
@@ -1,26 +1,37 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
 import qualified GI.Gtk as Gtk
 import Data.GI.Base
-import GI.Gtk.Objects.DrawingArea as GI
+import Record.Min
 
 activate :: Gtk.Application -> IO ()
 activate app = do
     box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
     win <- new Gtk.ApplicationWindow [#application := app, #title := "title", #child := box]
     button <- new Gtk.Button [#label := "name"]
-    on button #clicked $ putStrLn "fool"
-    #append box button
+    id #append box button
     doxx <- new Gtk.DrawingArea [#contentWidth := 100, #contentHeight := 100] 
-    GI.drawingAreaSetDrawFunc doxx (Just draw_func)
+    Gtk.drawingAreaSetDrawFunc doxx (Just draw_func)
     on doxx #resize $ \x -> \y -> putStrLn "test2"
-    #append box doxx
-    #show win
+    id #append box doxx
+    b <- new Gtk.ColorButton [#title := "rip"]
+    id #append box b
+    drag <- new Gtk.GestureDrag []
+    status <- new Gtk.ProgressBar []
+    on button #clicked $ #pulse status
+    id #append box status
+    id #show win
     where
     draw_func _ _ _ _ = putStrLn "test"
 
@@ -28,5 +39,5 @@ main :: IO ()
 main = do
     app <- new Gtk.Application [#applicationId := "edu.umich.hejohns.xournal"]
     on app #activate (activate app)
-    #run app Nothing
+    id #run app Nothing
     return ()
