3e5858a027a00147cf99face5282a32f6393f31e
Johnson He <hejohns@umich.edu>
2021-03-07 08:26:01 +0000

    cairo is gonna be a pain

--- Main.hs
+++ Main.hs
@@ -13,6 +13,8 @@ module Main (main) where
 
 import qualified GI.Gtk as Gtk
 import Data.GI.Base
+import qualified GI.Cairo as Cairo
+import qualified GI.Cairo.Render as Cairo.Render
 import Record.Min
 
 activate :: Gtk.Application -> IO ()
@@ -22,18 +24,26 @@ activate app = do
     button <- new Gtk.Button [#label := "name"]
     id #append box button
     doxx <- new Gtk.DrawingArea [#contentWidth := 100, #contentHeight := 100] 
-    Gtk.drawingAreaSetDrawFunc doxx (Just draw_func)
-    on doxx #resize $ \x -> \y -> putStrLn "test2"
+    --Gtk.drawingAreaSetDrawFunc doxx (Just draw_func)
+    id #setDrawFunc doxx (Just draw_func)
+    on doxx #resize $ \x y -> putStrLn "test2"
     id #append box doxx
     b <- new Gtk.ColorButton [#title := "rip"]
     id #append box b
-    drag <- new Gtk.GestureDrag []
+    stylus <- new Gtk.GestureStylus []
+    on stylus #motion drawing
+    on stylus #down $ \x y -> putStrLn "begin stylus"
     status <- new Gtk.ProgressBar []
     on button #clicked $ #pulse status
     id #append box status
     id #show win
     where
     draw_func _ _ _ _ = putStrLn "test"
+    drawing x y = do
+        --Cairo.imageSurfaceCreate
+        r <- new Cairo.Rectangle [#height := 50, #width := 50]
+        --Cairo.Render.fill
+        return () :: IO ()
 
 main :: IO ()
 main = do
